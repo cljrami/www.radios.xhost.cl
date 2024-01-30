59952ae6e7e46656bcdf6169bff55eb15f7c94af
jaime <cljrami@gmail.com>
2024-01-30 17:18:09 -0300

    oral

---
+++ src/components/NuestroDirectorio/SliderNuestroDirectorio.jsx
@@ -0,0 +1,76 @@
+// SWIPER
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import 'swiper/css/effect-coverflow';
+
+
+// import required modules
+import {  EffectCoverflow, Keyboard, Navigation } from "swiper/modules";
+
+const SliderNuestroDirectorio = () => {
+  return (
+    <div className="p-9">
+      <h2 className="-mb-4 font-bold text-lg/[26px] text-slate-200 ">Planes Xhost Radio</h2> 
+      <>      
+      
+      <Swiper
+      effect={'coverflow'}
+        slidesPerView={1}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 0,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        spaceBetween={0}
+        keyboard={{
+          enabled: true,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+        }}
+        navigation={true}
+        modules={[EffectCoverflow,Keyboard, Navigation]}
+        className="SliderNuestroDirectorio"
+      > 
+   
+        <SwiperSlide><img src="https://wallpapercosmos.com/w/full/3/7/f/1110343-2365x1571-desktop-hd-surfing-wallpaper-image.jpg"/></SwiperSlide>
+        <SwiperSlide><img src="https://wallpapercosmos.com/w/full/3/7/f/1110343-2365x1571-desktop-hd-surfing-wallpaper-image.jpg"/></SwiperSlide>
+        <SwiperSlide><img src="https://wallpapercosmos.com/w/full/3/7/f/1110343-2365x1571-desktop-hd-surfing-wallpaper-image.jpg"/></SwiperSlide>
+        <SwiperSlide><img src="https://wallpapercosmos.com/w/full/3/7/f/1110343-2365x1571-desktop-hd-surfing-wallpaper-image.jpg"/></SwiperSlide>
+        <SwiperSlide><img src="https://wallpapercosmos.com/w/full/3/7/f/1110343-2365x1571-desktop-hd-surfing-wallpaper-image.jpg"/></SwiperSlide>
+        <SwiperSlide><img src="https://wallpapercosmos.com/w/full/3/7/f/1110343-2365x1571-desktop-hd-surfing-wallpaper-image.jpg"/></SwiperSlide>
+        <SwiperSlide><img src="https://wallpapercosmos.com/w/full/3/7/f/1110343-2365x1571-desktop-hd-surfing-wallpaper-image.jpg"/></SwiperSlide>
+
+        <SwiperSlide><img src="https://wallpapercosmos.com/w/full/3/7/f/1110343-2365x1571-desktop-hd-surfing-wallpaper-image.jpg"/></SwiperSlide>
+       
+     </Swiper>  
+     </>
+    
+    </div>
+  )
+}
+
+export default SliderNuestroDirectorio
